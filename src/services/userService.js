a547b8c27aae0f3997f76ab04104d83b6e3e4dc6
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-05-04 18:15:29 -0300

    feat: add schema validation to user service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,15 @@
 import userModel from "../models/userModel.js";
 import AppError from "../utils/AppError.js";
+import {
+  getUserSchemaById,
+  createUserSchema,
+  updateUserSchema,
+  userDeleteSchema,
+} from "../Schema/userSchema.js";
 
 class UserService {
   async getAllUsers() {
-    return await userModel.getAll();
+    return await userModel.getAllUsers();
   }
 
   async getUserById(rawId) {
@@ -13,16 +19,20 @@ class UserService {
       throw new AppError("Invalid ID", 400);
     }
 
+    getUserSchemaById.parse({ id: rawId });
+
     const user = await userModel.getById(id);
 
     if (!user) {
-      throw new AppError("User not fould", 404);
+      throw new AppError("User not found", 404);
     }
 
     return user;
   }
 
   async createUser(userData) {
+    createUserSchema.parse(userData);
+
     const { email, username } = userData;
 
     const emailExists = userModel.users.find((u) => u.email === email);
@@ -32,18 +42,41 @@ class UserService {
       throw new AppError("User already exist", 409);
     }
 
-    const user = await userModel.create(userData);
+    const user = await userModel.createUser(userData);
     return user;
   }
 
   async updateUser(rawId, userData) {
-    const id = Number(rawId);
+    getUserSchemaById.parse({ id: rawId });
+    updateUserSchema.parse(userData);
 
-    if (isNaN(id)) {
+    if (isNaN(rawId)) {
       throw new AppError("Invalid ID", 400);
     }
 
-    const user = await userModel.update(id, userData);
+    const { email, username } = userData;
+
+    if (email) {
+      const emailExists = userModel.users.find(
+        (u) => u.email === email && u.id !== rawId,
+      );
+
+      if (emailExists) {
+        throw new AppError("Email already in use", 409);
+      }
+    }
+
+    if (username) {
+      const usernameExist = userModel.users.find(
+        (u) => u.username === username && u.id !== rawId,
+      );
+
+      if (usernameExist) {
+        throw new AppError("User name already in use", 409);
+      }
+    }
+
+    const user = await userModel.updateUser(rawId, userData);
 
     if (!user) {
       throw new AppError("User not found", 404);
@@ -53,6 +86,7 @@ class UserService {
 
   async deleteUser(rawId) {
     const id = Number(rawId);
+    userDeleteSchema.parse({ id: rawId });
 
     if (isNaN(id)) {
       throw new AppError("Invalid ID", 400);
@@ -61,7 +95,7 @@ class UserService {
     const user = await userModel.deleteUser(id);
 
     if (!user) {
-      throw new AppError("User not exist", 404);
+      throw new AppError("User not found", 404);
     }
 
     return user;
